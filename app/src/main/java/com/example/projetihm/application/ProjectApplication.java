7f8b87e1fd6997ce1293e803401b11062e562086
Gabriel C <gabriel.cogne@gmail.com>
2021-03-31 15:06:10 +0200

    Fix notification error

---
+++ app/src/main/java/com/example/projetihm/application/ProjectApplication.java
@@ -0,0 +1,31 @@
+package com.example.projetihm.application;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import java.util.Objects;
+
+/**
+ * @author Gabriel
+ */
+public class ProjectApplication extends Application {
+	private static final String CHANNEL_ID = Notification.CATEGORY_MESSAGE;
+
+	private void createNotificationChannel(String name, String description, int importance) {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+			NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+			channel.setDescription(description);
+			NotificationManager notificationManager = getSystemService(NotificationManager.class);
+			Objects.requireNonNull(notificationManager).createNotificationChannel(channel);
+		}
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		createNotificationChannel("Message", "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
+	}
+}
