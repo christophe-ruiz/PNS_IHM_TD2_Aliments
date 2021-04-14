044067f0326b84ba1d3a055e4757fba9b79142fd
Christophe Ruiz <christophe.ruiz@etu.univ-amu.fr>
2021-04-14 15:50:00 +0200

    OpenStreetMap implemented

--- app/src/main/java/com/example/projetihm/fragments/MapFragment.java
+++ app/src/main/java/com/example/projetihm/fragments/MapFragment.java
@@ -1,14 +1,29 @@
 package com.example.projetihm.fragments;
 
+import android.Manifest;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.projetihm.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,14 +32,7 @@ import com.example.projetihm.R;
  */
 public class MapFragment extends Fragment {
 
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
-
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
+	private MapView map;
 
 	public MapFragment() {
 		// Required empty public constructor
@@ -40,8 +48,6 @@ public class MapFragment extends Fragment {
 	public static MapFragment build() {
 		MapFragment fragment = new MapFragment();
 		Bundle args = new Bundle();
-		//args.putString(ARG_PARAM1, param1);
-		//args.putString(ARG_PARAM2, param2);
 		fragment.setArguments(args);
 		return fragment;
 	}
@@ -49,10 +55,7 @@ public class MapFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			//mParam1 = getArguments().getString(ARG_PARAM1);
-			//mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+		Configuration.getInstance().load(requireActivity().getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(requireActivity().getApplicationContext()));
 	}
 
 	@Override
@@ -60,5 +63,55 @@ public class MapFragment extends Fragment {
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_map, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		map = requireView().findViewById(R.id.map);
+		map.setTileSource(TileSourceFactory.MAPNIK);
+		requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+		GeoPoint startPoint = new GeoPoint(43.65, 7.00517);
+
+		IMapController mc = map.getController();
+		mc.setCenter(startPoint);
+		mc.setZoom(15.0);
+
+//		ArrayList<OverlayItem> items = new ArrayList<>();
+//		OverlayItem home = new OverlayItem("Test", "oui", new GeoPoint(43.65, 7.00517));
+//		items.add(home);
+//
+//		ItemizedOverlay<OverlayItem> mOverlay = new Ite<OverlayItem>(requireActivity().getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+//			@Override
+//			public boolean onItemSingleTapUp(int index, OverlayItem item) {
+//				return true;
+//			}
+//
+//			@Override
+//			public boolean onItemLongPress(int index, OverlayItem item) {
+//				return false;
+//			}
+//		});
+//
+//		mOverlay.setFocusItemsOnTap(true);
+//		map.getOverlays().add(mOverlay);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		map.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		map.onResume();
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		map = null;
 	}
 }
